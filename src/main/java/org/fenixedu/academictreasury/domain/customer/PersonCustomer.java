1ee43b7adb0519c8ef7446401a7b39bf84189b19
Anil Mamede <anil.mamede@qub-it.com>
2025-01-14 14:07:45 +0000

    WIP

--- src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
+++ src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
@@ -1,37 +1,27 @@
 /**
- * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/>
- * All rights reserved.
+ * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/> All rights reserved.
  *
- * Redistribution and use in source and binary forms, without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, without modification, are permitted provided that the following conditions
+ * are met:
  *
- * * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * * Neither the name of Quorum Born IT nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
- * * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade
- * de Lisboa (Departamento de Informática), hereby referred to as the Beneficiary, is the
- * sole demonstrated end-user and ultimately the only beneficiary of the redistributed binary
- * form and/or source code.
- * * The Beneficiary is entrusted with either the binary form, the source code, or both, and
- * by accepting it, accepts the terms of this License.
- * * Redistribution of any binary form and/or source code is only allowed in the scope of the
- * Universidade de Lisboa FenixEdu(™)’s implementation projects.
- * * This license and conditions of redistribution of source code/binary can only be reviewed
- * by the Steering Comittee of FenixEdu(™) <http://www.fenixedu.org/>.
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of Quorum Born IT nor
+ * the names of its contributors may be used to endorse or promote products derived from this software without specific prior
+ * written permission. * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade de Lisboa
+ * (Departamento de Informática), hereby referred to as the Beneficiary, is the sole demonstrated end-user and ultimately the only
+ * beneficiary of the redistributed binary form and/or source code. * The Beneficiary is entrusted with either the binary form,
+ * the source code, or both, and by accepting it, accepts the terms of this License. * Redistribution of any binary form and/or
+ * source code is only allowed in the scope of the Universidade de Lisboa FenixEdu(™)’s implementation projects. * This license
+ * and conditions of redistribution of source code/binary can only be reviewed by the Steering Comittee of FenixEdu(™)
+ * <http://www.fenixedu.org/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL “Quorum Born IT” BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL “Quorum Born IT” BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.fenixedu.academictreasury.domain.customer;
 
@@ -95,8 +85,8 @@ public class PersonCustomer extends PersonCustomer_Base {
         this();
 
         // TODO: CHECK IF THIS CODE DO ANYTHING
-        if (!DEFAULT_FISCAL_NUMBER.equals(getFiscalNumber())
-                && find(getPerson(), getAddressCountryCode(), getFiscalNumber()).count() > 1) {
+        if (!DEFAULT_FISCAL_NUMBER.equals(getFiscalNumber()) && find(getPerson(), getAddressCountryCode(),
+                getFiscalNumber()).count() > 1) {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.person.customer.duplicated");
         }
 
@@ -165,9 +155,8 @@ public class PersonCustomer extends PersonCustomer_Base {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.person.countryOfResidence.required");
         }
 
-        if (isActive()
-                && !TreasuryConstants.isSameCountryCode(services.fiscalAddress(getPerson()).getCountryOfResidence().getCode(),
-                        getAddressCountryCode())) {
+        if (isActive() && !TreasuryConstants.isSameCountryCode(
+                services.fiscalAddress(getPerson()).getCountryOfResidence().getCode(), getAddressCountryCode())) {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.countryOfResidence.is.not.equal.of.customer");
         }
 
@@ -180,8 +169,8 @@ public class PersonCustomer extends PersonCustomer_Base {
         //
         // Validate only of the customer is active
         if (isActive()) {
-            if (!TreasuryConstants.isDefaultCountry(getAddressCountryCode())
-                    || !DEFAULT_FISCAL_NUMBER.equals(getFiscalNumber())) {
+            if (!TreasuryConstants.isDefaultCountry(getAddressCountryCode()) || !DEFAULT_FISCAL_NUMBER.equals(
+                    getFiscalNumber())) {
                 final Set<Customer> customers = findByFiscalInformation(getAddressCountryCode(), getFiscalNumber()) //
                         .filter(c -> c.isPersonCustomer()) //
                         .filter(c -> c.isActive()) //
@@ -200,8 +189,8 @@ public class PersonCustomer extends PersonCustomer_Base {
 
         final Person person = isActive() ? getPerson() : getPersonForInactivePersonCustomer();
 
-        if (!DEFAULT_FISCAL_NUMBER.equals(getFiscalNumber())
-                && find(person, getAddressCountryCode(), getFiscalNumber()).filter(pc -> !pc.isFromPersonMerge()).count() > 1) {
+        if (!DEFAULT_FISCAL_NUMBER.equals(getFiscalNumber()) && find(person, getAddressCountryCode(), getFiscalNumber()).filter(
+                pc -> !pc.isFromPersonMerge()).count() > 1) {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.person.customer.duplicated");
         }
     }
@@ -465,8 +454,8 @@ public class PersonCustomer extends PersonCustomer_Base {
     public static boolean isValidFiscalNumber(final Person person) {
         IAcademicTreasuryPlatformDependentServices services = AcademicTreasuryPlataformDependentServicesFactory.implementation();
 
-        return services.fiscalAddress(person) != null
-                && FiscalCodeValidation.isValidFiscalNumber(addressCountryCode(person), fiscalNumber(person));
+        return services.fiscalAddress(person) != null && FiscalCodeValidation.isValidFiscalNumber(addressCountryCode(person),
+                fiscalNumber(person));
     }
 
     @Override
@@ -912,11 +901,11 @@ public class PersonCustomer extends PersonCustomer_Base {
     }
 
     // @formatter: off
+
     /************
      * SERVICES *
      ************/
     // @formatter: on
-
     public static Stream<? extends PersonCustomer> findAll() {
         return Customer.findAll().filter(c -> c instanceof PersonCustomer).map(PersonCustomer.class::cast);
     }
@@ -940,9 +929,10 @@ public class PersonCustomer extends PersonCustomer_Base {
 
     public static Stream<? extends PersonCustomer> find(final Person person, final String fiscalCountryCode,
             final String fiscalNumber) {
-        return find(person).filter(pc -> !Strings.isNullOrEmpty(pc.getAddressCountryCode())
-                && TreasuryConstants.isSameCountryCode(pc.getAddressCountryCode(), fiscalCountryCode)
-                && !Strings.isNullOrEmpty(pc.getFiscalNumber()) && pc.getFiscalNumber().equals(fiscalNumber));
+        return find(person).filter(
+                pc -> !Strings.isNullOrEmpty(pc.getAddressCountryCode()) && TreasuryConstants.isSameCountryCode(
+                        pc.getAddressCountryCode(), fiscalCountryCode) && !Strings.isNullOrEmpty(
+                        pc.getFiscalNumber()) && pc.getFiscalNumber().equals(fiscalNumber));
     }
 
     // ANIL 2024-12-16 (#qubIT-Fenix-6386)
@@ -962,8 +952,9 @@ public class PersonCustomer extends PersonCustomer_Base {
     // To resolve this issues, consider the active customer with highest precendence
     //
     // Also improve and shorten the comparator expression
-    public static final Comparator<PersonCustomer> IMPROVED_SORT_BY_PERSON_MERGE = Comparator.comparing(PersonCustomer::isActive)
-            .reversed().thenComparing(PersonCustomer::isFromPersonMerge).thenComparing(PersonCustomer::getExternalId);
+    public static final Comparator<PersonCustomer> IMPROVED_SORT_BY_PERSON_MERGE =
+            Comparator.comparing(PersonCustomer::isActive).reversed().thenComparing(PersonCustomer::isFromPersonMerge)
+                    .thenComparing(PersonCustomer::getExternalId);
 
     // ANIL 2024-12-16 (#qubIT-Fenix-6386)
     //
@@ -972,12 +963,6 @@ public class PersonCustomer extends PersonCustomer_Base {
 
     @Deprecated
     public static final Comparator<PersonCustomer> OLD_AND_DEPRECATED_SORT_BY_PERSON_MERGE = (o1, o2) -> {
-        if (o1.isActive() && !o2.isActive()) {
-            return -1;
-        } else if (!o1.isActive() && o2.isActive()) {
-            return 1;
-        }
-
         if (!o1.isFromPersonMerge() && o2.isFromPersonMerge()) {
             return -1;
         } else if (o1.isFromPersonMerge() && !o2.isFromPersonMerge()) {
@@ -1090,8 +1075,9 @@ public class PersonCustomer extends PersonCustomer_Base {
                 .flatMap(f2 -> f2.getPendingInvoiceEntriesSet().stream()).map(InvoiceEntry.class::cast)
                 .collect(Collectors.toSet());
 
-        final Comparator<? super InvoiceEntry> INVOICE_ENTRY_COMPARATOR = Comparator.comparing(InvoiceEntry::getDueDate)
-                .thenComparing(InvoiceEntry::getDescription).thenComparing(InvoiceEntry::getExternalId);
+        final Comparator<? super InvoiceEntry> INVOICE_ENTRY_COMPARATOR =
+                Comparator.comparing(InvoiceEntry::getDueDate).thenComparing(InvoiceEntry::getDescription)
+                        .thenComparing(InvoiceEntry::getExternalId);
 
         Set<InvoiceEntry> resultSorted = new TreeSet<InvoiceEntry>(INVOICE_ENTRY_COMPARATOR);
         resultSorted.addAll(invoiceEntries);
