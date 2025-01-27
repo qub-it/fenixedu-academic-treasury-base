129d4b5a56bb64968c0fade8dd5cf28283e83e3a
Anil Mamede <anil.mamede@qub-it.com>
2025-01-27 13:18:16 +0000

    - Bug Fix: Fix debts grabbing and setting finantial document, in debts aggregation strategy, run with debts generation rules [#qubIT-Fenix-6562]

--- src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/strategies/AggregateDebtsStrategy.java
+++ src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/strategies/AggregateDebtsStrategy.java
@@ -1,45 +1,33 @@
 /**
- * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/>
- * All rights reserved.
+ * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/> All rights reserved.
  *
- * Redistribution and use in source and binary forms, without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, without modification, are permitted provided that the following conditions
+ * are met:
  *
- * * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * * Neither the name of Quorum Born IT nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
- * * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade
- * de Lisboa (Departamento de Informática), hereby referred to as the Beneficiary, is the
- * sole demonstrated end-user and ultimately the only beneficiary of the redistributed binary
- * form and/or source code.
- * * The Beneficiary is entrusted with either the binary form, the source code, or both, and
- * by accepting it, accepts the terms of this License.
- * * Redistribution of any binary form and/or source code is only allowed in the scope of the
- * Universidade de Lisboa FenixEdu(™)’s implementation projects.
- * * This license and conditions of redistribution of source code/binary can only be reviewed
- * by the Steering Comittee of FenixEdu(™) <http://www.fenixedu.org/>.
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of Quorum Born IT nor
+ * the names of its contributors may be used to endorse or promote products derived from this software without specific prior
+ * written permission. * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade de Lisboa
+ * (Departamento de Informática), hereby referred to as the Beneficiary, is the sole demonstrated end-user and ultimately the only
+ * beneficiary of the redistributed binary form and/or source code. * The Beneficiary is entrusted with either the binary form,
+ * the source code, or both, and by accepting it, accepts the terms of this License. * Redistribution of any binary form and/or
+ * source code is only allowed in the scope of the Universidade de Lisboa FenixEdu(™)’s implementation projects. * This license
+ * and conditions of redistribution of source code/binary can only be reviewed by the Steering Comittee of FenixEdu(™)
+ * <http://www.fenixedu.org/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL “Quorum Born IT” BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL “Quorum Born IT” BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.fenixedu.academictreasury.domain.debtGeneration.strategies;
 
 import static org.fenixedu.academictreasury.domain.debtGeneration.IAcademicDebtGenerationRuleStrategy.findActiveDebitEntries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.fenixedu.academic.domain.DegreeCurricularPlan;
@@ -60,6 +48,7 @@ import org.fenixedu.academictreasury.services.IAcademicTreasuryPlatformDependent
 import org.fenixedu.academictreasury.services.TuitionServices;
 import org.fenixedu.treasury.domain.FinantialEntity;
 import org.fenixedu.treasury.domain.Product;
+import org.fenixedu.treasury.domain.debt.DebtAccount;
 import org.fenixedu.treasury.domain.document.DebitEntry;
 import org.fenixedu.treasury.domain.document.DebitNote;
 import org.fenixedu.treasury.domain.document.DocumentNumberSeries;
@@ -242,30 +231,31 @@ public class AggregateDebtsStrategy implements IAcademicDebtGenerationRuleStrate
             return;
         }
 
-        final DebitNote debitNote = grabPreparingOrCreateDebitNote(rule, debitEntries);
+        Map<DebtAccount, DebitNote> debitNotesMap = grabPreparingOrCreateDebitNote(rule, debitEntries);
 
         for (final DebitEntry debitEntry : debitEntries) {
             if (debitEntry.getFinantialDocument() == null) {
-                debitEntry.setFinantialDocument(debitNote);
+                DebtAccount debtAccountOwnerOfDebitNote =
+                        debitEntry.getPayorDebtAccount() != null ? debitEntry.getPayorDebtAccount() : debitEntry.getDebtAccount();
+                DebitNote debitNote = debitNotesMap.get(debtAccountOwnerOfDebitNote);
+                debitEntry.addToFinantialDocument(debitNote);
             }
 
-            if (debitNote.getPayorDebtAccount() == null && debitEntry.getPayorDebtAccount() != null) {
-                debitNote.updatePayorDebtAccount(debitEntry.getPayorDebtAccount());
-            }
-
-            if (debitEntry.getDebtAccount() != debitNote.getDebtAccount()) {
+            if (debitEntry.getDebtAccount() != debitEntry.getDebitNote().getDebtAccount()) {
                 throw new AcademicTreasuryDomainException(
                         "error.AcademicDebtGenerationRule.debit.entry.debtAccount.not.equal.debit.note.debtAccount");
             }
         }
 
-        if (debitNote.getFinantialDocumentEntriesSet().isEmpty()) {
-            throw new AcademicTreasuryDomainException("error.AcademicDebtGenerationRule.debit.note.without.debit.entries");
-        }
+        debitNotesMap.values().stream().filter(debitNote -> debitNote.getFinantialDocumentEntriesSet().isEmpty()).findAny()
+                .ifPresent(d -> {
+                    throw new AcademicTreasuryDomainException(
+                            "error.AcademicDebtGenerationRule.debit.note.without.debit.entries");
+                });
 
         if (rule.isAggregateAllOrNothing()) {
             for (final DebitEntry debitEntry : debitEntries) {
-                if (debitEntry.getFinantialDocument() != debitNote) {
+                if (!debitNotesMap.containsValue(debitEntry.getFinantialDocument())) {
                     throw new AcademicTreasuryDomainException(
                             "error.AcademicDebtGenerationRule.debit.entries.not.aggregated.on.same.debit.note");
                 }
@@ -274,7 +264,9 @@ public class AggregateDebtsStrategy implements IAcademicDebtGenerationRuleStrate
             // Check if all configured produts are in debitNote
             for (final Product product : rule.getAcademicDebtGenerationRuleEntriesSet().stream()
                     .map(AcademicDebtGenerationRuleEntry::getProduct).collect(Collectors.toSet())) {
-                if (debitNote.getDebitEntries().filter(l -> l.getProduct() == product).count() == 0) {
+
+                if (debitNotesMap.values().stream().flatMap(d -> d.getDebitEntriesSet().stream())
+                        .noneMatch(de -> de.getProduct() == product)) {
                     throw new AcademicTreasuryDomainException(
                             "error.AcademicDebtGenerationRule.debit.entries.not.aggregated.on.same.debit.note");
                 }
@@ -301,7 +293,15 @@ public class AggregateDebtsStrategy implements IAcademicDebtGenerationRuleStrate
         }
 
         if (t != null && t.isChargedWithDebitEntry()) {
-            return findActiveDebitEntries(personCustomer, t).filter(d -> d.isInDebt()).findFirst().orElse(null);
+            // ANIL 2025-01-27 (#qubIT-Fenix-6562)
+            //
+            // Instead of grabbing debit entries that are in debt,
+            // grab if they are preparing or without debit note.
+            // This will avoid fetching those already closed in
+            // debit note, and will fetch those that are totally
+            // exempted
+            return findActiveDebitEntries(personCustomer, t).filter(
+                    d -> d.getFinantialDocument() == null || d.getFinantialDocument().isPreparing()).findFirst().orElse(null);
         }
 
         return null;
@@ -331,29 +331,48 @@ public class AggregateDebtsStrategy implements IAcademicDebtGenerationRuleStrate
             return null;
         }
 
-        return findActiveDebitEntries(personCustomer, t, product).filter(d -> d.isInDebt()).findFirst().orElse(null);
+        // ANIL 2025-01-27 (#qubIT-Fenix-6562)
+        //
+        // Instead of grabbing debit entries that are in debt,
+        // grab if they are preparing or without debit note.
+        // This will avoid fetching those already closed in
+        // debit note, and will fetch those that are totally
+        // exempted
+        return findActiveDebitEntries(personCustomer, t, product).filter(
+                d -> d.getFinantialDocument() == null || d.getFinantialDocument().isPreparing()).findFirst().orElse(null);
     }
 
-    private DebitNote grabPreparingOrCreateDebitNote(final AcademicDebtGenerationRule rule, final Set<DebitEntry> debitEntries) {
+    // ANIL 2025-01-27 (#qubIT-Fenix-6562)
+    //
+    // Take into account the debit entries allocated for payor entities
+    private Map<DebtAccount, DebitNote> grabPreparingOrCreateDebitNote(final AcademicDebtGenerationRule rule,
+            final Set<DebitEntry> debitEntries) {
+        Map<DebtAccount, DebitNote> result = new HashMap<>();
 
         for (final DebitEntry debitEntry : debitEntries) {
             if (debitEntry.getFinantialDocument() != null && debitEntry.getFinantialDocument().isPreparing()) {
-                return (DebitNote) debitEntry.getFinantialDocument();
+                DebitNote debitNote = (DebitNote) debitEntry.getFinantialDocument();
+                DebtAccount ownerDebtAccount =
+                        debitNote.getPayorDebtAccount() != null ? debitNote.getPayorDebtAccount() : debitNote.getDebtAccount();
+
+                result.put(ownerDebtAccount, debitNote);
+            } else if (debitEntry.getFinantialDocument() == null) {
+                DebtAccount debtAccountOwnerOfDebitNote =
+                        debitEntry.getPayorDebtAccount() != null ? debitEntry.getPayorDebtAccount() : debitEntry.getDebtAccount();
+
+                FinantialEntity finantialEntity = debitEntry.getFinantialEntity();
+                DocumentNumberSeries defaultDocumentNumberSeries =
+                        DocumentNumberSeries.findUniqueDefaultSeries(FinantialDocumentType.findForDebitNote(), finantialEntity);
+
+                final DebitNote debitNote = DebitNote.create(finantialEntity, debitEntry.getDebtAccount(),
+                        debtAccountOwnerOfDebitNote != debitEntry.getDebtAccount() ? debtAccountOwnerOfDebitNote : null,
+                        defaultDocumentNumberSeries, new DateTime(), new LocalDate(), null, Collections.emptyMap(), null, null);
+
+                result.put(debtAccountOwnerOfDebitNote, debitNote);
             }
         }
 
-        DocumentNumberSeries defaultDocumentNumberSeries =
-                DocumentNumberSeries.findUniqueDefaultSeries(FinantialDocumentType.findForDebitNote(), rule.getFinantialEntity());
-        FinantialEntity finantialEntity = rule.getFinantialEntity();
-
-        if (finantialEntity == null && !debitEntries.isEmpty()) {
-            finantialEntity = debitEntries.iterator().next().getFinantialEntity();
-        }
-
-        final DebitNote debitNote = DebitNote.create(finantialEntity, debitEntries.iterator().next().getDebtAccount(), null,
-                defaultDocumentNumberSeries, new DateTime(), new LocalDate(), null, Collections.emptyMap(), null, null);
-
-        return debitNote;
+        return result;
     }
 
 }
--- src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/strategies/CreateDebtsStrategy.java
+++ src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/strategies/CreateDebtsStrategy.java
@@ -1,37 +1,27 @@
 /**
- * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/>
- * All rights reserved.
+ * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/> All rights reserved.
  *
- * Redistribution and use in source and binary forms, without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, without modification, are permitted provided that the following conditions
+ * are met:
  *
- * * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * * Neither the name of Quorum Born IT nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
- * * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade
- * de Lisboa (Departamento de Informática), hereby referred to as the Beneficiary, is the
- * sole demonstrated end-user and ultimately the only beneficiary of the redistributed binary
- * form and/or source code.
- * * The Beneficiary is entrusted with either the binary form, the source code, or both, and
- * by accepting it, accepts the terms of this License.
- * * Redistribution of any binary form and/or source code is only allowed in the scope of the
- * Universidade de Lisboa FenixEdu(™)’s implementation projects.
- * * This license and conditions of redistribution of source code/binary can only be reviewed
- * by the Steering Comittee of FenixEdu(™) <http://www.fenixedu.org/>.
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of Quorum Born IT nor
+ * the names of its contributors may be used to endorse or promote products derived from this software without specific prior
+ * written permission. * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade de Lisboa
+ * (Departamento de Informática), hereby referred to as the Beneficiary, is the sole demonstrated end-user and ultimately the only
+ * beneficiary of the redistributed binary form and/or source code. * The Beneficiary is entrusted with either the binary form,
+ * the source code, or both, and by accepting it, accepts the terms of this License. * Redistribution of any binary form and/or
+ * source code is only allowed in the scope of the Universidade de Lisboa FenixEdu(™)’s implementation projects. * This license
+ * and conditions of redistribution of source code/binary can only be reviewed by the Steering Comittee of FenixEdu(™)
+ * <http://www.fenixedu.org/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL “Quorum Born IT” BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL “Quorum Born IT” BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.fenixedu.academictreasury.domain.debtGeneration.strategies;
 
@@ -254,8 +244,7 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
         final Set<DebitEntry> debitEntries = Sets.newHashSet();
 
         DebitEntry grabbedDebitEntry = null;
-        for (final AcademicDebtGenerationRuleEntry entry : rule
-                .getOrderedAcademicDebtGenerationRuleEntries() /* rule.getAcademicDebtGenerationRuleEntriesSet() */) {
+        for (final AcademicDebtGenerationRuleEntry entry : rule.getOrderedAcademicDebtGenerationRuleEntries() /* rule.getAcademicDebtGenerationRuleEntriesSet() */) {
             final Product product = entry.getProduct();
 
             if (AcademicTreasurySettings.getInstance().getTuitionProductGroup() == product.getProductGroup()) {
@@ -283,8 +272,8 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
         for (final DebitEntry debitEntry : debitEntries) {
 
             if (debitEntry.getFinantialDocument() == null) {
-                DebitNote debitNote = debitNotesMap.get(debitEntry.getPayorDebtAccount() != null ? debitEntry
-                        .getPayorDebtAccount() : debitEntry.getDebtAccount());
+                DebitNote debitNote = debitNotesMap.get(
+                        debitEntry.getPayorDebtAccount() != null ? debitEntry.getPayorDebtAccount() : debitEntry.getDebtAccount());
                 debitEntry.addToFinantialDocument(debitNote);
             }
 
@@ -317,7 +306,6 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                     throw new AcademicTreasuryDomainException(
                             "error.AcademicDebtGenerationRule.debit.entries.not.aggregated.on.same.debit.note");
                 }
-
             }
         }
 
@@ -332,8 +320,9 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                 }
 
                 final Product interestProduct = TreasurySettings.getInstance().getInterestProduct();
-                final Set<DebitEntry> treasuryEventDebitEntries = DebitEntry.findActive(treasuryEvent)
-                        .filter(d -> d.getProduct() != interestProduct).collect(Collectors.<DebitEntry> toSet());
+                final Set<DebitEntry> treasuryEventDebitEntries =
+                        DebitEntry.findActive(treasuryEvent).filter(d -> d.getProduct() != interestProduct)
+                                .collect(Collectors.<DebitEntry> toSet());
 
                 if (!treasuryEventDebitEntries.equals(debitEntries)) {
                     throw new AcademicTreasuryDomainException("error.AcademicDebtGenerationRule.not.all.debitEntries.aggregated");
@@ -365,13 +354,14 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                 // where the student has to pay something, due to his short
                 // attendance in courses.
 
-                boolean forceCreation = entry.isCreateDebt() && entry.isForceCreation()
-                        && registration.getLastRegistrationState(executionYear) != null
-                        && registration.getLastRegistrationState(executionYear).isActive()
-                        && (!entry.isLimitToRegisteredOnExecutionYear() || registration.isFirstTime(rule.getExecutionYear()));
+                boolean forceCreation = entry.isCreateDebt() && entry.isForceCreation() && registration.getLastRegistrationState(
+                        executionYear) != null && registration.getLastRegistrationState(executionYear)
+                        .isActive() && (!entry.isLimitToRegisteredOnExecutionYear() || registration.isFirstTime(
+                        rule.getExecutionYear()));
 
-                boolean createdEnrolment = AcademicTaxServices.createAcademicTaxForEnrolmentDateAndDefaultFinantialEntity(
-                        registration, executionYear, academicTax, forceCreation);
+                boolean createdEnrolment =
+                        AcademicTaxServices.createAcademicTaxForEnrolmentDateAndDefaultFinantialEntity(registration,
+                                executionYear, academicTax, forceCreation);
 
                 if (createdEnrolment) {
                     processingResult.markRuleMadeUpdates();
@@ -425,10 +415,11 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                 // where the student has to pay something, due to his short
                 // attendance in courses.
 
-                boolean forceCreation = entry.isCreateDebt() && entry.isForceCreation() && isRegistrationToPayGratuities
-                        && registration.getLastRegistrationState(executionYear) != null
-                        && registration.getLastRegistrationState(executionYear).isActive()
-                        && (!entry.isLimitToRegisteredOnExecutionYear() || registration.isFirstTime(rule.getExecutionYear()));
+                boolean forceCreation =
+                        entry.isCreateDebt() && entry.isForceCreation() && isRegistrationToPayGratuities && registration.getLastRegistrationState(
+                                executionYear) != null && registration.getLastRegistrationState(executionYear)
+                                .isActive() && (!entry.isLimitToRegisteredOnExecutionYear() || registration.isFirstTime(
+                                rule.getExecutionYear()));
 
                 if (entry.isToCreateAfterLastRegistrationStateDate()) {
                     final LocalDate lastRegisteredStateDate = TuitionServices.lastRegisteredDate(registration, executionYear);
@@ -447,8 +438,9 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                     }
                 } else {
                     final LocalDate enrolmentDate = TuitionServices.enrolmentDate(registration, executionYear, forceCreation);
-                    boolean createdTuitions = TuitionServices.createInferedTuitionForRegistration(registration, executionYear,
-                            enrolmentDate, forceCreation);
+                    boolean createdTuitions =
+                            TuitionServices.createInferedTuitionForRegistration(registration, executionYear, enrolmentDate,
+                                    forceCreation);
 
                     if (createdTuitions) {
                         processingResult.markRuleMadeUpdates();
@@ -519,8 +511,9 @@ public class CreateDebtsStrategy implements IAcademicDebtGenerationRuleStrategy 
                         debitEntry.getPayorDebtAccount() != null ? debitEntry.getPayorDebtAccount() : debitEntry.getDebtAccount();
 
                 if (result.get(debtAccountOwnerOfDebitNote) == null) {
-                    DocumentNumberSeries documentNumberSeries = DocumentNumberSeries
-                            .findUniqueDefaultSeries(FinantialDocumentType.findForDebitNote(), rule.getFinantialEntity());
+                    DocumentNumberSeries documentNumberSeries =
+                            DocumentNumberSeries.findUniqueDefaultSeries(FinantialDocumentType.findForDebitNote(),
+                                    rule.getFinantialEntity());
 
                     DebitNote debitNote = DebitNote.create(debitEntry.getFinantialEntity(), debitEntry.getDebtAccount(), null,
                             documentNumberSeries, new DateTime(), new LocalDate(), null, Collections.emptyMap(), null, null);
