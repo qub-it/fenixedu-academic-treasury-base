69fba32874b3120ea4ec42e4f1d6d0bfbe841410
Anil Mamede <anil.mamede@qub-it.com>
2025-02-05 16:20:16 +0000

    - Refactor: Move FenixEduAcademicTreasuryPlatformDependentServices#finantialEntityOfDegree to AcademicTreasuryConstants [#qubIT-Fenix-6593]

--- src/main/java/org/fenixedu/academictreasury/util/AcademicTreasuryConstants.java
+++ src/main/java/org/fenixedu/academictreasury/util/AcademicTreasuryConstants.java
@@ -1,46 +1,42 @@
 /**
- * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/>
- * All rights reserved.
+ * Copyright (c) 2015, Quorum Born IT <http://www.qub-it.com/> All rights reserved.
  *
- * Redistribution and use in source and binary forms, without modification, are permitted
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, without modification, are permitted provided that the following conditions
+ * are met:
  *
- * * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
- * provided with the distribution.
- * * Neither the name of Quorum Born IT nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written
- * permission.
- * * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade
- * de Lisboa (Departamento de Informática), hereby referred to as the Beneficiary, is the
- * sole demonstrated end-user and ultimately the only beneficiary of the redistributed binary
- * form and/or source code.
- * * The Beneficiary is entrusted with either the binary form, the source code, or both, and
- * by accepting it, accepts the terms of this License.
- * * Redistribution of any binary form and/or source code is only allowed in the scope of the
- * Universidade de Lisboa FenixEdu(™)’s implementation projects.
- * * This license and conditions of redistribution of source code/binary can only be reviewed
- * by the Steering Comittee of FenixEdu(™) <http://www.fenixedu.org/>.
+ * * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of Quorum Born IT nor
+ * the names of its contributors may be used to endorse or promote products derived from this software without specific prior
+ * written permission. * Universidade de Lisboa and its respective subsidiary Serviços Centrais da Universidade de Lisboa
+ * (Departamento de Informática), hereby referred to as the Beneficiary, is the sole demonstrated end-user and ultimately the only
+ * beneficiary of the redistributed binary form and/or source code. * The Beneficiary is entrusted with either the binary form,
+ * the source code, or both, and by accepting it, accepts the terms of this License. * Redistribution of any binary form and/or
+ * source code is only allowed in the scope of the Universidade de Lisboa FenixEdu(™)’s implementation projects. * This license
+ * and conditions of redistribution of source code/binary can only be reviewed by the Steering Comittee of FenixEdu(™)
+ * <http://www.fenixedu.org/>.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL “Quorum Born IT” BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL “Quorum Born IT” BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.fenixedu.academictreasury.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.fenixedu.academic.domain.Degree;
+import org.fenixedu.academic.domain.organizationalStructure.Unit;
 import org.fenixedu.academictreasury.domain.serviceRequests.ITreasuryServiceRequest;
+import org.fenixedu.treasury.domain.FinantialEntity;
 import org.fenixedu.treasury.dto.TreasuryTupleDataSourceBean;
 import org.fenixedu.treasury.services.integration.TreasuryPlataformDependentServicesFactory;
 import org.fenixedu.commons.i18n.LocalizedString;
@@ -80,12 +76,12 @@ public class AcademicTreasuryConstants {
     static {
         SELECT_OPTION = new TreasuryTupleDataSourceBean();
         SELECT_OPTION.setId("");
-        
+
         try {
             SELECT_OPTION.setText(academicTreasuryBundle("label.TreasuryTupleDataSourceBean.select.description"));
-        } catch(Throwable t) {
+        } catch (Throwable t) {
             t.printStackTrace();
-            
+
             SELECT_OPTION.setText("Select Option");
         }
     }
@@ -119,11 +115,11 @@ public class AcademicTreasuryConstants {
     }
 
     // @formatter: off
+
     /**************
      * MATH UTILS *
      **************/
     // @formatter: on
-
     public static boolean isNegative(final BigDecimal value) {
         return !isZero(value) && !isPositive(value);
     }
@@ -149,18 +145,18 @@ public class AcademicTreasuryConstants {
     }
 
     // @formatter: off
+
     /**********
      * BUNDLE *
      **********/
     // @formatter: on
-
     public static String academicTreasuryBundle(final String key, final String... args) {
         return TreasuryPlataformDependentServicesFactory.implementation().bundle(AcademicTreasuryConstants.BUNDLE, key, args);
     }
 
     public static String academicTreasuryBundle(final Locale locale, final String key, final String... args) {
-        return TreasuryPlataformDependentServicesFactory.implementation().bundle(locale, AcademicTreasuryConstants.BUNDLE, key,
-                args);
+        return TreasuryPlataformDependentServicesFactory.implementation()
+                .bundle(locale, AcademicTreasuryConstants.BUNDLE, key, args);
     }
 
     public static LocalizedString academicTreasuryBundleI18N(final String key, final String... args) {
@@ -172,15 +168,15 @@ public class AcademicTreasuryConstants {
     }
 
     // @formatter: off
+
     /**************
      * DATE UTILS *
      **************/
     // @formatter: on
-
     public static boolean isDateBetween(final LocalDate beginDate, final LocalDate endDate, final LocalDate when) {
         return new Interval(beginDate.toDateTimeAtStartOfDay(),
-                endDate != null ? endDate.toDateTimeAtStartOfDay().plusDays(1).minusSeconds(1) : INFINITY_DATE)
-                        .contains(when.toDateTimeAtStartOfDay());
+                endDate != null ? endDate.toDateTimeAtStartOfDay().plusDays(1).minusSeconds(1) : INFINITY_DATE).contains(
+                when.toDateTimeAtStartOfDay());
     }
 
     public static boolean isDateBetween(final LocalDate beginDate, final LocalDate endDate, final DateTime when) {
@@ -206,8 +202,52 @@ public class AcademicTreasuryConstants {
             numberOfEnrolments +=
                     request.hasExtracurricularEnrolmentsByYear() ? request.getExtracurricularEnrolmentsByYear().size() : 0;
 
-            return numberOfApprovedExtraCurriculum + numberOfApprovedStandaloneCurriculum + numberOfApprovedEnrolments
-                    + numberOfCurriculum + numberOfEnrolments;
+            return numberOfApprovedExtraCurriculum + numberOfApprovedStandaloneCurriculum + numberOfApprovedEnrolments + numberOfCurriculum + numberOfEnrolments;
         }
+    }
+
+    /*
+     * This method returns the Finantial Entity responsible for the degree.
+     *
+     * This method is used to get the academic tariffs associated with the Finantial Entity.
+     * Also it is used to get the finantial institution, in order to know the debt account associated
+     * with the customer.
+     *
+     * For tuitions it is different, in the sense that the TuitionPaymentPlan is obtained with
+     * DegreeCurricularPlan. With the TuitionPaymentPlan we get the FinantialEntity.
+     *
+     */
+    public static FinantialEntity getFinantialEntityOfDegree(Degree degree, LocalDate when) {
+        // Look at the organizational structure
+        Unit degreeUnit = degree.getUnit();
+
+        if (degreeUnit == null) {
+            return null;
+        }
+
+        List<FinantialEntity> candidateFinantialEntities =
+                degreeUnit.getAllParentUnits().stream().map(parent -> parent.getFinantialEntity()).filter(Objects::nonNull)
+                        .sorted(FinantialEntity.COMPARE_BY_NAME).collect(Collectors.toList());
+
+        if (candidateFinantialEntities.size() == 1) {
+            // There is no ambiguity. The degree descendent of one unit
+            // associated with the finantial entity
+
+            return candidateFinantialEntities.iterator().next();
+        } else if (candidateFinantialEntities.size() > 1) {
+            // The degree is descendent of more than one unit
+            // associated with the finantial entity
+            //
+            // We need to untie and find which finantial entity
+            // is responsible for finantial entity
+
+            // TODO: Find with a specific accountability type?
+            // It is not desirable to return the first ordered
+            // alphabetically
+
+            return candidateFinantialEntities.iterator().next();
+        }
+
+        return null;
     }
 }
