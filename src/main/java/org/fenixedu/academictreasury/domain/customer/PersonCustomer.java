b27bd76517572ecffd68381d6757a86c0b63ed36
Anil Mamede <anil.mamede@qub-it.com>
2026-07-13 12:23:20 +0100

    - Bug Fix: Improve error message of existing customer with the same fiscal number [#qubIT-Fenix-8990]

--- src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
+++ src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
@@ -179,8 +179,7 @@ public class PersonCustomer extends PersonCustomer_Base {
                     final Set<String> otherCustomers =
                             customers.stream().filter(c -> c != self).map(c -> c.getName()).collect(Collectors.<String> toSet());
 
-                    throw new TreasuryDomainException("error.Customer.customer.with.fiscal.information.exists",
-                            Joiner.on(", ").join(otherCustomers));
+                    throw new TreasuryDomainException("error.Customer.customer.with.fiscal.information.exists");
                 }
             }
         }
